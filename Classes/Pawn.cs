5ddbaccfd4c9ee691277c1ccbbe8aaae2e558698
STEPS0N <stepan.klimoff@mail.ru>
2025-09-21 23:54:38 +0500

    Добавлена новая фигура

--- Classes/Pawn.cs
+++ Classes/Pawn.cs
@@ -15,60 +15,118 @@ namespace Pr4.Classes
         public int X {  get; set; }
         public int Y { get; set; }
         public bool Select = false;
+        public bool IsQueen = false;
         public bool Black = false;
         public Grid Figure {  get; set; }
-        public Pawn(int X, int Y, bool Black)
+        public Pawn(int X, int Y, bool Black, bool isQueen = false)
         {
             this.X = X;
             this.Y = Y;
             this.Black = Black;
+            this.IsQueen = isQueen;
         }
 
         public void SelectFigure(object sender, MouseButtonEventArgs e)
         {
             bool attack = false;
             Pawn SelectPawn = MainWindow.mainWindow.Pawns.Find(x => x.Select == true);
-            if (SelectPawn != null)
+            Pawn SelectQueen = MainWindow.mainWindow.Queen.Find(x => x.Select == true);
+            if (SelectPawn != null && SelectPawn != this)
             {
-                if (this.Black && this.Y - 1 == SelectPawn.Y && (this.X - 1 == SelectPawn.X || this.X == SelectPawn.X || this.X + 1 == SelectPawn.X) ||
-                    !this.Black && this.Y + 1 == SelectPawn.Y && (this.X - 1 == SelectPawn.X || this.X == SelectPawn.X || this.X + 1 == SelectPawn.X))
+                if ((SelectPawn.Black && SelectPawn.Y - 1 == this.Y && (SelectPawn.X - 1 == this.X || SelectPawn.X + 1 == this.X)) ||
+                    (!SelectPawn.Black && SelectPawn.Y + 1 == this.Y && (SelectPawn.X - 1 == this.X || SelectPawn.X + 1 == this.X)))
                 {
+                    if (this.IsQueen)
+                    {
+                        MainWindow.mainWindow.Queen.Remove(this);
+                    }
+                    else
+                    {
+                        MainWindow.mainWindow.Pawns.Remove(this);
+                    }
                     MainWindow.mainWindow.gameBoard.Children.Remove(this.Figure);
                     Grid.SetColumn(SelectPawn.Figure, this.X);
                     Grid.SetRow(SelectPawn.Figure, this.Y);
                     SelectPawn.X = this.X;
                     SelectPawn.Y = this.Y;
-                    SelectPawn.SelectFrigure(null, null);
-                    attack = true;
+                    SelectPawn.Select = false;
+                    MainWindow.mainWindow.IsWhiteTurn = !MainWindow.mainWindow.IsWhiteTurn;
+                    return;
+                }
+            }
+
+            if (SelectQueen != null && SelectQueen != this)
+            {
+                if (this.Black != SelectQueen.Black && (this.X == SelectQueen.X || this.Y == SelectQueen.Y || Math.Abs(this.X - SelectQueen.X) == Math.Abs(this.Y - SelectQueen.Y)))
+                {
+                    if (this.IsQueen)
+                    {
+                        MainWindow.mainWindow.Queen.Remove(this);
+                    }
+                    else
+                    {
+                        MainWindow.mainWindow.Pawns.Remove(this);
+                    }
+                    MainWindow.mainWindow.gameBoard.Children.Remove(this.Figure);
+                    Grid.SetColumn(SelectQueen.Figure, this.X);
+                    Grid.SetRow(SelectQueen.Figure, this.Y);
+                    SelectQueen.X = this.X;
+                    SelectQueen.Y = this.Y;
+                    SelectQueen.Select = false;
+                    MainWindow.mainWindow.IsWhiteTurn = !MainWindow.mainWindow.IsWhiteTurn;
+                    return;
                 }
             }
 
             if (!attack)
             {
-                MainWindow.mainWindow.OnSelect(this);
-                if (this.Select)
+                if (!this.IsQueen)
                 {
-                    if (this.Black)
+                    MainWindow.mainWindow.OnSelect(this, this);
+                    if (this.Select)
                     {
-                        this.Figure.Background = new ImageBrush(new BitmapImage(new Uri(@"Images/Pawn (black).png")));
+                        if (this.Black)
+                        {
+                            this.Figure.Background = new ImageBrush(new BitmapImage(new Uri(@"pack://application:,,,/Images/Pawn (black).png")));
+                        }
+                        else
+                        {
+                            this.Figure.Background = new ImageBrush(new BitmapImage(new Uri(@"pack://application:,,,/Images/Pawn.png")));
+                        }
+                        this.Select = false;
                     }
                     else
                     {
-                        this.Figure.Background = new ImageBrush(new BitmapImage(new Uri(@"Images/Pawn (white).png")));
+                        this.Figure.Background = new ImageBrush(new BitmapImage(new Uri(@"pack://application:,,,/Images/Pawn (select).png")));
+                        this.Select = true;
                     }
-                    this.Select = false;
                 }
                 else
                 {
-                    this.Figure.Background = new ImageBrush(new BitmapImage(new Uri(@"Images/Pawn (select).png")));
-                    this.Select = true;
+                    if (this.Select)
+                    {
+                        if (this.Black)
+                        {
+                            this.Figure.Background = new ImageBrush(new BitmapImage(new Uri(@"pack://application:,,,/Images/Queen (black).png")));
+                        }
+                        else
+                        {
+                            this.Figure.Background = new ImageBrush(new BitmapImage(new Uri(@"pack://application:,,,/Images/Queen (white).png")));
+                        }
+                        this.Select = false;
+                    }
+                    else
+                    {
+                        this.Figure.Background = new ImageBrush(new BitmapImage(new Uri(@"pack://application:,,,/Images/Queen (select).png")));
+                        this.Select = true;
+                    }
                 }
             }
         }
 
-        public void Transform(int X,  int Y)
+        public void TransformPawns(int X,  int Y)
         {
-            if (X != this.X)
+            if (X != this.X || (this.Black && MainWindow.mainWindow.IsWhiteTurn) || (!this.Black && !MainWindow.mainWindow.IsWhiteTurn))
             {
                 SelectFigure(null, null);
                 return;
@@ -80,8 +138,28 @@ namespace Pr4.Classes
                 Grid.SetRow(this.Figure, Y);
                 this.X = X;
                 this.Y = Y;
+                MainWindow.mainWindow.IsWhiteTurn = !MainWindow.mainWindow.IsWhiteTurn;
             }
             SelectFigure(null, null);
         }
+
+        public void TransformQueen(int X, int Y)
+        {
+            if ((this.Black && MainWindow.mainWindow.IsWhiteTurn) || (!this.Black && !MainWindow.mainWindow.IsWhiteTurn))
+            {
+                SelectFigure(null, null);
+                return;
+            }
+            if (Math.Abs(X - this.X) == Math.Abs(Y - this.Y) || X == this.X || Y == this.Y)
+            {
+                Grid.SetColumn(this.Figure, X);
+                Grid.SetRow(this.Figure, Y);
+                this.X = X;
+                this.Y = Y;
+                MainWindow.mainWindow.IsWhiteTurn = !MainWindow.mainWindow.IsWhiteTurn;
+            }
+            SelectFigure(null, null);
+        }
+
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Pr4.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,30 +22,38 @@ namespace Pr4
     public partial class MainWindow : Window
     {
         public static MainWindow mainWindow;
+        public bool IsWhiteTurn = true;
         public List<Classes.Pawn> Pawns = new List<Classes.Pawn>();
+        public List<Classes.Pawn> Queen = new List<Classes.Pawn>();
 
         public MainWindow()
         {
             InitializeComponent();
             MainWindow.mainWindow = this;
 
-            Pawns.Add(new Classes.Pawn(0, 1, false));
-            Pawns.Add(new Classes.Pawn(1, 1, false));
-            Pawns.Add(new Classes.Pawn(2, 1, false));
-            Pawns.Add(new Classes.Pawn(3, 1, false));
-            Pawns.Add(new Classes.Pawn(4, 1, false));
-            Pawns.Add(new Classes.Pawn(5, 1, false));
-            Pawns.Add(new Classes.Pawn(6, 1, false));
-            Pawns.Add(new Classes.Pawn(7, 1, false));
+            Pawns.Add(new Classes.Pawn(0, 1, false, false));
+            Pawns.Add(new Classes.Pawn(1, 1, false, false));
+            Pawns.Add(new Classes.Pawn(2, 1, false, false));
+            Pawns.Add(new Classes.Pawn(3, 1, false, false));
+            Pawns.Add(new Classes.Pawn(4, 1, false, false));
+            Pawns.Add(new Classes.Pawn(5, 1, false, false));
+            Pawns.Add(new Classes.Pawn(6, 1, false, false));
+            Pawns.Add(new Classes.Pawn(7, 1, false, false));
 
-            Pawns.Add(new Classes.Pawn(0, 6, false));
-            Pawns.Add(new Classes.Pawn(1, 6, false));
-            Pawns.Add(new Classes.Pawn(2, 6, false));
-            Pawns.Add(new Classes.Pawn(3, 6, false));
-            Pawns.Add(new Classes.Pawn(4, 6, false));
-            Pawns.Add(new Classes.Pawn(5, 6, false));
-            Pawns.Add(new Classes.Pawn(6, 6, false));
-            Pawns.Add(new Classes.Pawn(7, 6, false));
+            Pawns.Add(new Classes.Pawn(0, 6, true, false));
+            Pawns.Add(new Classes.Pawn(1, 6, true, false));
+            Pawns.Add(new Classes.Pawn(2, 6, true, false));
+            Pawns.Add(new Classes.Pawn(3, 6, true, false));
+            Pawns.Add(new Classes.Pawn(4, 6, true, false));
+            Pawns.Add(new Classes.Pawn(5, 6, true, false));
+            Pawns.Add(new Classes.Pawn(6, 6, true, false));
+            Pawns.Add(new Classes.Pawn(7, 6, true, false));
+
+            Queen.Add(new Classes.Pawn(3, 0, false, true));
+
+            Queen.Add(new Classes.Pawn(3, 7, true, true));
+
+            CreateFigure();
         }
 
         public void CreateFigure()
@@ -58,11 +67,31 @@ namespace Pr4
                 };
                 if (Pawn.Black)
                 {
-                    Pawn.Figure.Background = new ImageBrush(new BitmapImage(new Uri(@"Images/Pawn (black).png")));
+                    Pawn.Figure.Background = new ImageBrush(new BitmapImage(new Uri(@"pack://application:,,,/Images/Pawn (black).png")));
                 }
                 else
                 {
-                    Pawn.Figure.Background = new ImageBrush(new BitmapImage(new Uri(@"Images/Pawn (white).png")));
+                    Pawn.Figure.Background = new ImageBrush(new BitmapImage(new Uri(@"pack://application:,,,/Images/Pawn.png")));
+                }
+                Grid.SetColumn(Pawn.Figure, Pawn.X);
+                Grid.SetRow(Pawn.Figure, Pawn.Y);
+                Pawn.Figure.MouseDown += Pawn.SelectFigure;
+                gameBoard.Children.Add(Pawn.Figure);
+            }
+            foreach (Classes.Pawn Pawn in Queen)
+            {
+                Pawn.Figure = new Grid()
+                {
+                    Width = 50,
+                    Height = 50
+                };
+                if (Pawn.Black)
+                {
+                    Pawn.Figure.Background = new ImageBrush(new BitmapImage(new Uri(@"pack://application:,,,/Images/Queen (black).png")));
+                }
+                else
+                {
+                    Pawn.Figure.Background = new ImageBrush(new BitmapImage(new Uri(@"pack://application:,,,/Images/Queen (white).png")));
                 }
                 Grid.SetColumn(Pawn.Figure, Pawn.X);
                 Grid.SetRow(Pawn.Figure, Pawn.Y);
@@ -71,16 +100,21 @@ namespace Pr4
             }
         }
 
-        public void OnSelect(Classes.Pawn SelectPawn)
+        public void OnSelect(Classes.Pawn SelectPawn, Classes.Pawn SelectQueen)
         {
             foreach (Classes.Pawn Pawn in Pawns)
             {
-                if (Pawn != SelectPawn)
+                if (Pawn != SelectPawn && Pawn.Select)
                 {
-                    if (Pawn.Select)
-                    {
-                        Pawn.SelectFigure(null, null);
-                    }
+                    Pawn.SelectFigure(null, null);
+                }
+            }
+
+            foreach (Classes.Pawn Pawn in Queen)
+            {
+                if (Pawn != SelectQueen && Pawn.Select)
+                {
+                    Pawn.SelectFigure(null, null);
                 }
             }
         }
@@ -91,10 +125,16 @@ namespace Pr4
             int X = Grid.GetColumn(Tile);
             int Y = Grid.GetRow(Tile);
             Classes.Pawn SelectPawn = Pawns.Find(x => x.Select == true);
+            Classes.Pawn SelectQueen = Queen.Find(x => x.Select == true);
             if (SelectPawn != null)
             {
-                SelectPawn.Transform(X, Y);
+                SelectPawn.TransformPawns(X, Y);
+            }
+            else if (SelectQueen != null)
+            {
+                SelectQueen.TransformQueen(X, Y);
             }
         }
+
     }
 }
